b5be0309d77f992a22426a3b3bb22b0fce7ea487
smartZZD <1396896729@qq.com>
2020-06-05 12:51:20 +0800

    项目结构初始化

---
+++ gulimall-ware/src/test/java/com/zzd/gulimall/ware/GulimallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzd.gulimall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
